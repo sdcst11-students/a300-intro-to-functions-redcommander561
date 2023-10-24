e33597aac8f8afe348e57205faf480554f86ea35
redcommander561 <126519219+redcommander561@users.noreply.github.com>
2023-10-24 13:00:46 -0700

    doneskies

--- task1.py
+++ task1.py
@@ -1,6 +1,21 @@
 def A():
-
+    print("hello")
 def B():
-
+    print("how are you")
 
 def C():
+    print("hi")
+
+ 
+
+Input = input(" Enter the capital letter A, B, or C")
+
+if Input == "A":
+    A()
+
+if Input == "B":
+    B()
+
+if Input == "C":
+    C()
+
--- task2.py
+++ task2.py
@@ -8,3 +8,21 @@ displays instructions and how to play
 game()
 plays the games
 """
+import random
+
+def title():
+    print("pick a number 1 through 10: ")
+
+def game():
+    guess = int(input(""))
+    randNum = int(random.randint(0,10))
+    if guess == randNum:
+        print(f"you got it! the number was {randNum}")
+    
+    
+    else:
+        print(f"wrong, the number was {randNum} ")
+
+title()
+
+game()
